396b57aada3800b44b66e09dcb2657e9215a3412
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-13 16:42:19 -0300

    create context for shopping cart

--- src/routes.js
+++ src/routes.js
@@ -3,30 +3,33 @@ import Feira from "pages/Feira";
 import Login from "pages/Login";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { UsuarioProvider } from "common/context/Usuario";
+import { CarrinhoProvider } from "common/context/Carrinho";
 
 function Router() {
 
-  return (
-    <BrowserRouter>
-      <Switch>
+	return (
+		<BrowserRouter>
+			<Switch>
 
-        <UsuarioProvider>
-          <Route exact path="/">
-              <Login />
-          </Route>
+			<UsuarioProvider>
+				<Route exact path="/">
+					<Login />
+				</Route>
+				
+				<CarrinhoProvider>
+					<Route path="/feira">
+						<Feira />
+					</Route>
+				</CarrinhoProvider>				
+			</UsuarioProvider>
 
-          <Route path="/feira">
-            <Feira />
-          </Route>
-        </UsuarioProvider>
-
-        <Route path="/carrinho">
-          <Carrinho />
-        </Route>
-        
-      </Switch>
-    </BrowserRouter>
-  )
-}
+			<Route path="/carrinho">
+				<Carrinho />
+			</Route>
+			
+			</Switch>
+		</BrowserRouter>
+	)
+	}
 
 export default Router;
